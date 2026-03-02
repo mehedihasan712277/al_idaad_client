44850eb0e0d32c3010013b2eb89d0119762d300a
mehedi <mehedihasan712277@gmail.com>
2026-03-02 10:16:29 +0600

    feat: blog details page responsive

--- src/app/blog/details/[id]/page.tsx
+++ src/app/blog/details/[id]/page.tsx
@@ -1,6 +1,8 @@
+import { getBlogs } from "@/utils/fetchData";
 import { formatMongoDate } from "@/utils/helper";
 import { BlogType, GetSingleBlogResponseType } from "@/utils/types";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 300;
 
@@ -20,6 +22,7 @@ async function getSingleBlog(id: string): Promise<BlogType> {
 const BlogDetailsPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     const { id } = await params;
     const blog = await getSingleBlog(id);
+    const allBlog = await getBlogs();
 
     if (!blog) {
         return <div>error</div>;
@@ -27,17 +30,31 @@ const BlogDetailsPage = async ({ params }: { params: Promise<{ id: string }> }) 
     const { title, description, category, createdAt, thumbnail } = blog as BlogType;
 
     return (
-        <div className="max-w-200 mx-auto space-y-4 px-4 py-20">
-            <h1 className="text-3xl font-bold text-main">{title}</h1>
-
-            <div className="flex justify-between items-center">
-                <p>Written on : {formatMongoDate(createdAt)}</p>
-                <p>Category : {category.name}</p>
+        <div className="max-w-5xl xl:max-w-7xl mx-auto py-20 px-4 lg:flex lg:justify-between">
+            <div className="space-y-3 border-r border-border pr-8 max-w-60 xl:max-w-80 hidden lg:block">
+                <p className="font-bold">All Blogs</p>
+                {allBlog.map((ele) => (
+                    <p
+                        key={ele._id}
+                        className={` ${title === ele.title ? "text-brand" : "text-text_normal hover:underline transition duration-150"}`}
+                    >
+                        <Link href={`/blog/details/${ele._id}`}>{ele.title}</Link>
+                    </p>
+                ))}
             </div>
+            <div className="max-w-200 mx-auto lg:mx-0 lg:max-w-180 xl:max-w-200 space-y-4">
+                <h1 className="text-3xl font-bold text-main">{title}</h1>
 
-            <Image src={thumbnail} alt={title} width={800} height={450} className="w-200 h-112.5 rounded" />
+                <div className="flex flex-wrap justify-between items-center">
+                    <p>{formatMongoDate(createdAt)}</p>
+                    <p className="text-brand">{category.name}</p>
+                </div>
 
-            <div dangerouslySetInnerHTML={{ __html: description }} className="text-soft ProseMirror" />
+                <div className="w-full lg:w-200 xl:w-200 aspect-video relative rounded overflow-hidden">
+                    <Image src={thumbnail} alt={title} fill className="object-cover" />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: description }} className="text-soft ProseMirror" />
+            </div>
         </div>
     );
 };
