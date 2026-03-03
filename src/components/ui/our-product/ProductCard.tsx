c23a6d115ddaa10084998a89f4d67808f933eccb
mehedi <mehedihasan712277@gmail.com>
2026-03-03 17:57:32 +0600

    feat: add zoom effect to product

--- src/components/ui/our-product/ProductCard.tsx
+++ src/components/ui/our-product/ProductCard.tsx
@@ -11,13 +11,15 @@ const ProductCard = ({ data }: { data: Prouct }) => {
     const { url, title, price, category } = data;
     return (
         <div className="space-y-1 w-[calc(50vw-32px)] md:w-[calc(33vw-24px)] lg:w-[calc(25vw-24px)] xl:w-75 relative box-border">
-            <Image
-                src={url}
-                alt="product"
-                width={300}
-                height={450}
-                className="w-[calc(50vw-32px)] md:w-[calc(33vw-24px)] lg:w-[calc(25vw-24px)] xl:w-75 aspect-2/3 rounded"
-            ></Image>
+            <div className="w-[calc(50vw-32px)] md:w-[calc(33vw-24px)] lg:w-[calc(25vw-24px)] xl:w-75 aspect-2/3 rounded overflow-hidden">
+                <Image
+                    src={url}
+                    alt="product"
+                    width={300}
+                    height={450}
+                    className="w-[calc(50vw-32px)] md:w-[calc(33vw-24px)] lg:w-[calc(25vw-24px)] xl:w-75 aspect-2/3 rounded hover:scale-120 transition ease-in-out duration-250"
+                ></Image>
+            </div>
             <div className="px-1">
                 <p className="font-semibold">{title}</p>
                 <div className="flex items-center justify-between">
