b1fd45458393d977808a6a4a26402af2a8fbeb2f
mehedi <mehedihasan712277@gmail.com>
2026-03-06 10:01:47 +0600

    changed offer banner

--- src/components/ui/offer/OfferSlider.tsx
+++ src/components/ui/offer/OfferSlider.tsx
@@ -13,6 +13,7 @@ import "swiper/css/effect-fade";
 // import required modules
 import { EffectFade, Autoplay } from "swiper/modules";
 import Image from "next/image";
+import Link from "next/link";
 
 const OfferSlider = () => {
     return (
@@ -34,21 +35,23 @@ const OfferSlider = () => {
                 className="mySwiper"
             >
                 {[
-                    { img: "https://i.postimg.cc/V5PC9DSM/offer-main.png" },
-                    { img: "https://i.postimg.cc/DwgXsQ7Q/offer.png" },
+                    { img: "https://i.postimg.cc/1XBknN5f/offer-banner-1.png" },
+                    { img: "https://i.postimg.cc/pL4gFvxy/offer-banner-2.png" },
                     // { img: "https://i.postimg.cc/kMtp7Gy1/offer-t.jpg" },
                     // { img: "https://i.postimg.cc/HkdKM9pK/thobe-4.jpg" },
                 ].map((speaker, index) => (
                     <SwiperSlide key={index}>
                         {/* Image */}
-                        <div className="flex justify-center relative h-[30vw] xl:h-95">
-                            <Image
-                                src={speaker.img}
-                                alt="al idaad offer product"
-                                width={1920}
-                                height={1068}
-                                className="absolute left-0 right-0 h-[30vw] xl:h-95 rounded"
-                            />
+                        <div className="flex justify-center relative w-full aspect-5/2">
+                            <Link href={`/all-products`}>
+                                <Image
+                                    src={speaker.img}
+                                    alt="al idaad offer product"
+                                    width={1920}
+                                    height={1068}
+                                    className="absolute left-0 right-0 w-full aspect-5/2 rounded"
+                                />
+                            </Link>
                         </div>
                     </SwiperSlide>
                 ))}
